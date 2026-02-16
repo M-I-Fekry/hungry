1fd7e5d8d4d2a3783feb9c0c70447e987a42dd96
Fekry <me031230@gmail.com>
2026-02-16 23:37:04 +0200

    fix(shared): fix syntax typo in CustomButton.

--- lib/shared/custom_button.dart
+++ lib/shared/custom_button.dart
@@ -4,8 +4,18 @@ import 'package:hungry_app/core/constants/app_colors.dart';
 import 'package:hungry_app/shared/custom_text.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, this.onTap});
+  const CustomButton({
+    super.key,
+    required this.text,
+    this.onTap,
+    required this.width,
+    required this.height,
+    required this.size,
+  });
   final String text;
+  final double width;
+  final double height;
+  final double size;
   final Function()? onTap;
 
   @override
@@ -13,8 +23,8 @@ class CustomButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 200,
-        height: 70,
+        width: width,
+        height: height,
         decoration: BoxDecoration(
           color: AppColors.primaryColor,
           borderRadius: BorderRadius.circular(16),
@@ -23,12 +33,11 @@ class CustomButton extends StatelessWidget {
           child: CustomText(
             text: text,
             color: Colors.white,
-            size: 20,
+            size: size,
             weight: FontWeight.bold,
           ),
         ),
       ),
     );
-    ;
   }
 }
