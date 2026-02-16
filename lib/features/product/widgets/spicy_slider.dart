6ce4e8855e1fc406dac3cb9eedf4ff3fccaa87d3
Fekry <me031230@gmail.com>
2026-02-16 16:55:57 +0200

    feat(product): create SpicySlider widget.

---
+++ lib/features/product/widgets/spicy_slider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry_app/core/constants/app_colors.dart';
+import 'package:hungry_app/shared/custom_text.dart';
+
+class SpicySlider extends StatefulWidget {
+  const SpicySlider({super.key, required this.onChanged, required this.value});
+  final double value;
+  final ValueChanged<double> onChanged;
+
+  @override
+  State<SpicySlider> createState() => _SpicySliderState();
+}
+
+class _SpicySliderState extends State<SpicySlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset('assets/detail/sandwitch_detail.png', height: 275),
+        Spacer(),
+
+        Column(
+          children: [
+            CustomText(
+              text:
+                  'Customize Your Burger\n to Your Tastes.\n Ultimate Experience',
+            ),
+            Slider(
+              min: 0,
+              max: 1,
+              value: widget.value,
+              onChanged: widget.onChanged,
+              inactiveColor: Colors.grey.shade300,
+              activeColor: AppColors.primaryColor,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                CustomText(text: '🥶'),
+                Gap(100),
+                CustomText(text: '🌶️'),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
