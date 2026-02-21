83b7691cfb651452ab25c2808c76d7214f31b0c3
Fekry <me031230@gmail.com>
2026-02-21 13:33:47 +0200

    ui(auth): redesign SignupView layout and integrate SignupForm.

--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry_app/core/constants/app_colors.dart';
+import 'package:hungry_app/features/auth/view/login_view.dart';
 import 'package:hungry_app/features/auth/widgets/custom_btn.dart';
+import 'package:hungry_app/features/auth/widgets/signup_form.dart';
 import 'package:hungry_app/shared/custom_text.dart';
 import 'package:hungry_app/shared/custom_text_field.dart';
 
@@ -15,62 +17,43 @@ class SignupView extends StatefulWidget {
 }
 
 class _SignupViewState extends State<SignupView> {
-  final TextEditingController emailControlle = TextEditingController();
-  final TextEditingController nameControlle = TextEditingController();
-  final TextEditingController passControlle = TextEditingController();
-  final TextEditingController confirmPassControlle = TextEditingController();
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primaryColor,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: AppColors.primaryColor,
+        body: Center(
+          child: Column(
+            children: [
+              const Gap(80),
+              SvgPicture.asset('assets/logo/logo.svg', color: Colors.white),
+              const Gap(10),
+              const CustomText(
+                text: 'Create New Account',
+                color: Colors.white,
+                size: 18,
+                weight: FontWeight.w500,
+              ),
+              const Gap(40),
 
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                Gap(100),
-                SvgPicture.asset('assets/logo/logo.svg'),
-                Gap(10),
-                Gap(60),
-                CustomTextField(
-                  controlle: nameControlle,
-                  hintText: 'Name',
-                  isPassword: false,
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 30,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                  child: SignupForm(),
                 ),
-                Gap(15),
-                CustomTextField(
-                  controlle: emailControlle,
-                  hintText: 'Email Address',
-                  isPassword: false,
-                ),
-                Gap(15),
-                CustomTextField(
-                  controlle: passControlle,
-                  hintText: 'Password',
-                  isPassword: true,
-                ),
-                Gap(15),
-                CustomTextField(
-                  controlle: confirmPassControlle,
-                  hintText: 'Confirm Password',
-                  isPassword: true,
-                ),
-                Gap(30),
-                CustomAuthBtn(
-                  onTap: () {
-                    if (formKey.currentState!.validate()) {
-                      print('success login');
-                    }
-                  },
-                  text: 'Sign up',
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
