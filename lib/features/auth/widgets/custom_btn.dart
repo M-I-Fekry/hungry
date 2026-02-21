6cb824b357cdfe62da55de3814db530fe3a50b16
Fekry <me031230@gmail.com>
2026-02-21 12:00:36 +0200

    feat(ui): add optional background and text colors to CustomAuthBtn.

--- lib/features/auth/widgets/custom_btn.dart
+++ lib/features/auth/widgets/custom_btn.dart
@@ -4,11 +4,18 @@ import 'package:hungry_app/core/constants/app_colors.dart';
 import 'package:hungry_app/shared/custom_text.dart';
 
 class CustomAuthBtn extends StatelessWidget {
-  const CustomAuthBtn({super.key, required this.text, this.onTap});
+  const CustomAuthBtn({
+    super.key,
+    required this.text,
+    this.onTap,
+    this.btnColor,
+    this.textColor,
+  });
 
   final Function()? onTap;
-
   final String text;
+  final Color? btnColor;
+  final Color? textColor;
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +25,14 @@ class CustomAuthBtn extends StatelessWidget {
         height: 45,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          color: Colors.white,
+          color: btnColor ?? Colors.white,
         ),
-
         width: double.infinity,
         child: Center(
           child: CustomText(
-            text:text,
+            text: text,
             weight: FontWeight.bold,
-            color: AppColors.primaryColor,
+            color: textColor ?? AppColors.primaryColor,
           ),
         ),
       ),
