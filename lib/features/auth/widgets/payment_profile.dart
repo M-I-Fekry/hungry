3aab9d2992b39103623e0829f850439f64416f60
Fekry <me031230@gmail.com>
2026-02-21 11:56:15 +0200

    feat(auth): implement PaymentProfile card widget for user payment info.

---
+++ lib/features/auth/widgets/payment_profile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hungry_app/core/constants/app_colors.dart';
+import 'package:hungry_app/shared/custom_text.dart';
+
+class PaymentProfile extends StatelessWidget {
+  const PaymentProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+      tileColor: AppColors.primaryColor,
+      leading: Image.asset('assets/icon/profileVisa.png', width: 50),
+      title: CustomText(text: 'Debit Card', color: Colors.white),
+      subtitle: CustomText(text: '8880 **** **** 8088', color: Colors.white),
+      trailing: Radio<String>(
+        activeColor: Colors.white,
+        value: 'visa',
+        groupValue: 'visa',
+        onChanged: (value) => {},
+      ),
+      onTap: () {},
+    );
+  }
+}
