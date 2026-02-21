11cf21d4a53881eec0d93f13883b0f120835b0ab
Fekry <me031230@gmail.com>
2026-02-21 12:14:47 +0200

    ui(auth): redesign LoginView layout and integrate LoginForm.

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -2,21 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry_app/core/constants/app_colors.dart';
-import 'package:hungry_app/features/auth/widgets/custom_btn.dart';
+import 'package:hungry_app/features/auth/widgets/login_form.dart';
 import 'package:hungry_app/shared/custom_text.dart';
-import 'package:hungry_app/shared/custom_text_field.dart';
 
-class LoginView extends StatefulWidget {
+class LoginView extends StatelessWidget {
   const LoginView({super.key});
-
-  @override
-  State<LoginView> createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<LoginView> {
-  TextEditingController emailControlle = TextEditingController();
-  TextEditingController passWordControlle = TextEditingController();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -24,48 +14,36 @@ class _LoginViewState extends State<LoginView> {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         backgroundColor: AppColors.primaryColor,
-
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Gap(100),
-                  SvgPicture.asset('assets/logo/logo.svg'),
-                  Gap(10),
-                  CustomText(
-                    text: 'Hello,M_I_Fekry',
-                    color: Colors.white,
-                    size: 18,
-                    weight: FontWeight.w500,
-                  ),
-                  Gap(60),
-                  CustomTextField(
-                    controlle: emailControlle,
-                    hintText: 'Email Address',
-                    isPassword: false,
-                  ),
-                  Gap(20),
-                  CustomTextField(
-                    controlle: passWordControlle,
-                    hintText: 'Password',
-                    isPassword: true,
-                  ),
-                  Gap(55),
-
-                  CustomAuthBtn(
-                    onTap: () {
-                      if (_formKey.currentState!.validate()) {
-                        print('success register');
-                      }
-                    },
-                    text: 'Sign up',
-                  ),
-                ],
+          child: Column(
+            children: [
+              const Gap(100),
+              SvgPicture.asset('assets/logo/logo.svg', color: Colors.white),
+              const Gap(10),
+              const CustomText(
+                text: 'Login to your account',
+                color: Colors.white,
+                size: 18,
+                weight: FontWeight.w500,
               ),
-            ),
+              const Gap(60),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 40,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                  child: const LoginForm(),
+                ),
+              ),
+            ],
           ),
         ),
       ),
