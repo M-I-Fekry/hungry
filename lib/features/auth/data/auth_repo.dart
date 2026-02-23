9522432af7f64429b1d0dc7a08804fa11f63f3b4
Fekry <me031230@gmail.com>
2026-02-23 23:57:29 +0200

    feat(auth): complete login flow with API integration and UI connection.

---
+++ lib/features/auth/data/auth_repo.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:hungry_app/core/network/api_error.dart';
+import 'package:hungry_app/core/network/api_exceptions.dart';
+import 'package:hungry_app/core/network/api_service.dart';
+import 'package:hungry_app/core/utils/pref_helpers.dart';
+import 'package:hungry_app/features/auth/data/auth_model.dart';
+
+class AuthRepo {
+  ApiService apiService = ApiService();
+  bool isGuest = false;
+  UserModel? _currentUser;
+
+  //Login
+  Future<UserModel?> login(String email, String password) async {
+    try {
+      final response = await apiService.post('/login', {
+        'email': email,
+        'password': password,
+      });
+      if (response is ApiError) {
+        throw response;
+      }
+
+      if (response is Map<String, dynamic>) {
+        final msg = response['message'];
+        final code = response['code'];
+        final data = response['data'];
+
+        print('📡 Login response - code: $code, data: $data');
+
+        if (code != 200 && code != 201) {
+          throw ApiError(message: msg ?? 'Unknown error');
+        }
+
+        final user = UserModel.fromJson(data);
+        print('🔐 Login successful - User token: ${user.token ?? 'null'}');
+
+        if (user.token != null) {
+          await PrefHelpers.savetoken(user.token!);
+          print('💾 Token saved to storage: ${user.token}');
+        } else {
+          print('⚠️ No token received from server!');
+        }
+
+        isGuest = false;
+        _currentUser = user;
+        return user;
+      } else {
+        throw ApiError(message: 'UnExpected Error From Server');
+      }
+    } on DioError catch (e) {
+      throw ApiExceptions.handleError(e);
+    } catch (e) {
+      throw ApiError(message: e.toString());
+    }
+  }
+
+}
--- lib/features/auth/widgets/login_form.dart
+++ lib/features/auth/widgets/login_form.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry_app/core/constants/app_colors.dart';
+import 'package:hungry_app/core/network/api_error.dart';
+import 'package:hungry_app/features/auth/data/auth_repo.dart';
 import 'package:hungry_app/features/auth/view/signup_view.dart';
 import 'package:hungry_app/features/auth/widgets/auth_switch_text.dart';
 import 'package:hungry_app/features/auth/widgets/continue_guest_text.dart';
 import 'package:hungry_app/features/auth/widgets/custom_btn.dart';
 import 'package:hungry_app/root.dart';
+import 'package:hungry_app/shared/custom_snack.dart';
 import 'package:hungry_app/shared/custom_text_field.dart';
 
 class LoginForm extends StatefulWidget {
@@ -19,6 +23,28 @@ class _LoginFormState extends State<LoginForm> {
   final TextEditingController emailControlle = TextEditingController();
   final TextEditingController passWordControlle = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  AuthRepo authRepo = AuthRepo();
+
+  Future<void> login() async {
+    if (!_formKey.currentState!.validate()) return;
+    setState(() => isLoading = true);
+
+    try {
+      final user = await authRepo.login(
+        emailControlle.text.trim(),
+        passWordControlle.text.trim(),
+      );
+      if (user != null)
+        Navigator.push(context, MaterialPageRoute(builder: (_) => Root()));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        customSnack(e is ApiError ? e.message : 'Unhandled login error'),
+      );
+    } finally {
+      setState(() => isLoading = false);
+    }
+  }
 
   @override
   void dispose() {
@@ -31,52 +57,52 @@ class _LoginFormState extends State<LoginForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          CustomTextField(
-            controlle: emailControlle,
-            hintText: 'Email Address',
-            isPassword: false,
-            fillColor: Colors.grey.shade200,
-            borderColor: Colors.transparent,
-          ),
-          const Gap(20),
-          CustomTextField(
-            controlle: passWordControlle,
-            hintText: 'Password',
-            isPassword: true,
-            fillColor: Colors.grey.shade200,
-            borderColor: Colors.transparent,
-          ),
-          const Gap(40),
-          CustomAuthBtn(
-            onTap: () {
-              if (_formKey.currentState!.validate()) {
-                print('success login');
-              }
-            },
-            text: 'login',
-            btnColor: AppColors.primaryColor,
-            textColor: Colors.white,
-          ),
-          const Gap(20),
-          AuthSwitchText(
-            text: "Don't have an account? ",
-            actionText: "Sign Up",
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (c) {
-                    return SignupView();
-                  },
-                ),
-              );
-            },
-          ),
-          const Gap(15),
-          ContinueGuestText(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(
+              controlle: emailControlle,
+              hintText: 'Email Address',
+              isPassword: false,
+              fillColor: Colors.grey.shade200,
+              borderColor: Colors.transparent,
+            ),
+            const Gap(20),
+            CustomTextField(
+              controlle: passWordControlle,
+              hintText: 'Password',
+              isPassword: true,
+              fillColor: Colors.grey.shade200,
+              borderColor: Colors.transparent,
+            ),
+            const Gap(40),
+            isLoading
+                ? CupertinoActivityIndicator(color: Colors.black)
+                : CustomAuthBtn(
+                    onTap: login,
+                    text: 'login',
+                    btnColor: AppColors.primaryColor,
+                    textColor: Colors.white,
+                  ),
+            const Gap(20),
+            AuthSwitchText(
+              text: "Don't have an account? ",
+              actionText: "Sign Up",
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (c) {
+                      return SignupView();
+                    },
+                  ),
+                );
+              },
+            ),
+            const Gap(15),
+            ContinueGuestText(),
+          ],
+        ),
       ),
     );
   }
