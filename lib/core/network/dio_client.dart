2f7638d2a6a3f477c798b523bfd0343a5be379b6
Fekry <me031230@gmail.com>
2026-02-23 23:44:50 +0200

    feat(network): setup DioClient with base url and interceptors.

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:hungry_app/core/utils/pref_helpers.dart';
+
+class DioClient {
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://sonic-zdi0.onrender.com/api',
+      headers: {"Content-Type": "application/json"},
+    ),
+  );
+
+  DioClient() {
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler)async {
+          final token =await PrefHelpers.getToken();
+
+          if (token != null && token.isNotEmpty && token != 'guest') {
+            options.headers['Authorization'] = 'Bearer $token';
+            print('Authorization header added');
+          } else {
+            print('No authorization header added');
+          }
+          return handler.next(options);
+        },
+      ),
+    );
+  }
+
+  Dio get dio => _dio;
+}
